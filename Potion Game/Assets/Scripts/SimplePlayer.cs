8e1628e9a379a657f84868644ddc67e9dd2be1cb
Matthew Jackson <102381833@student.swin.edu.au>
2021-04-07 16:27:51 +1000

    + added the kinematic character controller library and started writing the basic character and camera movement scripts.

---
+++ Potion Game/Assets/Scripts/SimplePlayer.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// This is where the player and camera inputs are handled. 
+public class SimplePlayer : MonoBehaviour
+{
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
