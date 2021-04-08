99f26bc4235a3ee3f81ba991ff0d94ec199b7fa2
Matthew Jackson <47026654+thisismatthew@users.noreply.github.com>
2021-04-08 11:14:21 +1000

    + minor changes to early character and camera controllers

--- Potion Game/Assets/Scripts/GamePadCameraController.cs
+++ Potion Game/Assets/Scripts/GamePadCameraController.cs
@@ -1,14 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using KinematicCharacterController;
 
-// This is where the player and camera inputs are handled. 
-public class SimplePlayer : MonoBehaviour
+public class GamePadCameraController : MonoBehaviour
 {
-    public SimpleCharacterController character;
-    public GamePadCameraController characterCamera;
-   
     // Start is called before the first frame update
     void Start()
     {
