bdba1cb51b2638b25507c7d276d830b8c2e80159
ThisIsMatthew <102381833@student.swin.edu.au>
2021-09-23 20:20:16 +1000

    FIX: volume slider now works for main music

--- Potion Game/Assets/Scripts/UI/OptionsHelper.cs
+++ Potion Game/Assets/Scripts/UI/OptionsHelper.cs
@@ -6,10 +6,23 @@ public class OptionsHelper : MonoBehaviour
 {
     public bool Fullscreen, GamepadController;
     public float MasterVolume;
+    private float _currentVolume;
+    private AudioManager _audioManager;
     private void Start()
     {
          DontDestroyOnLoad(transform.gameObject);
+        _audioManager = FindObjectOfType<AudioManager>();
     }
 
-
+    private void Update()
+    {
+        if (_currentVolume != MasterVolume)
+        {
+            _currentVolume = MasterVolume;
+            foreach(Sound s in _audioManager.sounds)
+            {
+                s.source.volume = _currentVolume;
+            }
+        }
+    }
 }
