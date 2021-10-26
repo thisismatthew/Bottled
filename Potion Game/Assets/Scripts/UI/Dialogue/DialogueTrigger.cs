c0bf652646ad9964d74179926a1842368f1481fb
ThisIsMatthew <102381833@student.swin.edu.au>
2021-10-26 12:53:44 +1100

    FIX: swapped the trigger enter for trigger stay for dialogue events

--- Potion Game/Assets/Scripts/UI/Dialogue/DialogueTrigger.cs
+++ Potion Game/Assets/Scripts/UI/Dialogue/DialogueTrigger.cs
@@ -43,7 +43,7 @@ public class DialogueTrigger : MonoBehaviour
     }
 
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerStay(Collider other)
     {
         if (other.CompareTag("DialogueEvent")&& !InsideDialogue)
         {
